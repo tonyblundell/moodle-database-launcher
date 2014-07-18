d589fefae3f53f82d36b4dab0c227c7a3562048f
Tony Blundell <tony.blundell@kineo.com>
2014-07-18 15:33:34 +0100

    tidy code

--- moodle-db-launcher.py
+++ moodle-db-launcher.py
@@ -9,20 +9,34 @@ import re
 import subprocess
 
 def main():
-    path = os.path.join(os.getcwd(), 'config.php')
+    path = get_config_file_path(os.getcwd())
     connection_info = get_connection_info(path)
     launch_database(connection_info)
 
+def get_config_file_path(path):
+    file_path = os.path.join(path, 'config.php')
+    if os.path.isfile(file_path):
+        return file_path
+    path = os.path.sep.join(path.split(os.path.sep)[:-1])
+    if path:
+        return get_config_file_path(path)
+    raise Exception('Config file not found')
+
 def get_connection_info(path):
+    settings = ('dbtype', 'dbname', 'dbuser', 'dbpass')
     connection_info = {}
     with open(path) as open_file:
         for line in open_file.readlines():
-            for setting in ('dbtype', 'dbname', 'dbuser', 'dbpass'):
-                pattern = '{setting}.*\'(.+)\''.format(setting=setting)
+            for setting in settings:
+                pattern = '{setting}.*[\'\"](.+)[\'\"]'.format(setting=setting)
                 matches = re.search(pattern, line)
                 if matches:
                     connection_info[setting] = matches.groups()[0]
-    return connection_info
+    diff = set(settings) - set(connection_info.keys())
+    if not diff:
+        return connection_info
+    missing = ', '.join(diff) 
+    raise Exception('Settings not found in config file: {}'.format(missing))
 
 def launch_database(connection_info):
     if connection_info['dbtype'] == 'pgsql':
